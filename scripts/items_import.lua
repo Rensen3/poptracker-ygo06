753dc81960f07cdb782c7f23a2f70dfbe6c224e7
Rensen3 <renesmdt@gmail.com>
2025-01-07 12:12:19 +0100

    add first booster

---
+++ scripts/items_import.lua
@@ -0,0 +1,6 @@
+
+Tracker:AddItems("items/items.jsonc")
+Tracker:AddItems("items/hint_items.jsonc")
+Tracker:AddItems("items/location_items.jsonc")
+Tracker:AddItems("items/labels.jsonc")
+                
---
+++ scripts/layouts_import.lua
@@ -0,0 +1,9 @@
+
+Tracker:AddLayouts("layouts/events.jsonc")
+Tracker:AddLayouts("layouts/settings_popup.jsonc")
+Tracker:AddLayouts("layouts/items.jsonc")
+Tracker:AddLayouts("layouts/tabs.jsonc")
+Tracker:AddLayouts("layouts/tracker.jsonc")
+Tracker:AddLayouts("layouts/broadcast.jsonc")
+Tracker:AddLayouts("layouts/dungeon_items.jsonc")
+-- Tracker:AddLayouts("layouts/dungeon_items_keydrop.json")
